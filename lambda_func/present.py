ae89e287265c58e281d1afaaf7bd4a6e58ae4b46
Rinatius <rinat@kloop.kg>
2018-06-22 14:52:00 +0600

    Make result string pretty

--- lambda_func/present.py
+++ lambda_func/present.py
@@ -1,2 +1,22 @@
+import pandas as pd
+import strings
+
 def transform(data):
-    return str(data)
+    return (dataframe_to_str(data) if isinstance(data, pd.DataFrame) else str(data))
+
+def dataframe_to_str(dataframe):
+    print(dataframe)
+    columns = list(dataframe.columns.values)
+    result = strings.general_strings['found_upon_request'] + ' ' + column_to_str(columns[0], 1) + ': ' + strings.general_strings['in_total'] + str(len(dataframe.index)) + '\n'
+    for index, row in dataframe.iterrows():
+        for column in columns:
+            result = result + column_to_str(column, 0) + ': ' + row[column] + '/n'
+        result = result + '/n'
+    return result
+        
+def column_to_str(column, index=0):
+    if column in strings.tables_columns:
+        result = strings.tables_columns[column][index]
+    else:
+        result = column
+    return result
