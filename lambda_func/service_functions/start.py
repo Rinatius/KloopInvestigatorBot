205fbeb6b35df1424cc27f6c2de4a1a45a9e3078
xakim <khakim.davuro@gmail.com>
2019-04-23 12:26:39 -0700

    Delete bot_start_testing, Modify start.py

--- lambda_func/service_functions/start.py
+++ lambda_func/service_functions/start.py
@@ -1,26 +1,9 @@
-import json
-import constants as c
-import snip
-from outgoing import message
-from user import User
+import strings as s
 
 
 def serve(user):
 
-    greetings = 'Вас приветствует Бот бла бла бла ...!'
-    user.update_service_messages(greetings)
-    message(user)
+    user.update_service_messages(s.sheets_undecided_service_message)
 
 
-def start_handler(event, context):
 
-    body = event[c.REQUEST_BODY]
-    data = json.loads(body)
-    user_id = data[c.REQUEST_USER]
-
-    response = snip.response200
-    response[c.RESPONSE_BODY] = {}
-
-    user = User(user_id)
-
-    serve(user)
